c3419e714a32cf3ce7bb757fdc35a21a7943883e
Ishrat Jahan Tisha <47575896+tisha7@users.noreply.github.com>
2026-09-07 19:35:30 +0000

    Create portfolio foundation

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll('a[href^="#"]');
+
+  links.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      const targetId = link.getAttribute("href");
+
+      if (targetId === "#") return;
+
+      const target = document.querySelector(targetId);
+
+      if (target) {
+        event.preventDefault();
+        target.scrollIntoView({
+          behavior: "smooth",
+          block: "start"
+        });
+      }
+    });
+  });
+});
